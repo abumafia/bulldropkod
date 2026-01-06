75ce0899129d1a40f422cb0722e02ed39216ef48
HALLAYM LLC <158750555+abumafia@users.noreply.github.com>
2026-01-06 22:08:54 +0500

    Update bd.js

--- bd.js
+++ bd.js
@@ -3,7 +3,6 @@ const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config();
 
-
 // Express server yaratish
 const app = express();
 const PORT = process.env.PORT || 3000;
